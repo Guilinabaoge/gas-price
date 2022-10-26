01b5b49b30374be01aba269a17dc5745fd447ed6
Guilinabaoge <42078245+Guilinabaoge@users.noreply.github.com>
2022-10-27 01:41:05 +0200

    check point

--- src/Marks.js
+++ src/Marks.js
@@ -1,4 +1,6 @@
-import { geoMercator, geoPath } from 'd3';
+import { geoMercator, geoPath,select } from 'd3';
+import './styles.css'
+import React from 'react';
 
 const w = 960;
 const h = 100;
@@ -7,6 +9,15 @@ const projection = geoMercator()
 	.center([10.40, 53.31])
   .scale([w]);
 const path = geoPath(projection);
+
+// function sayHi(){
+//   console.log('hi')
+// }
+
+function setState(state){
+  document.getElementById('statename').textContent = state;
+  // console.log(name.textContent)
+}
 
 export const Marks = ({
   map,
@@ -17,10 +28,7 @@ export const Marks = ({
   <g className="marks">
     {map.features.map(feature => {
       const d = rowByState.get(feature.properties.NAME_1)
-      // console.log(d)
-      // console.log(colorValue(d))
-      // console.log(colorScale(colorValue(d)))
-      return <path fill={colorScale(colorValue(d))} d={path(feature)}/> 
+      return <path fill={colorScale(colorValue(d))} d={path(feature)} onClick={()=>setState(feature.properties.NAME_1)}/> 
     })}
     
   </g>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React, { StrictMode,useState, useCallback, useEffect } from 'react';
 import { createRoot } from "react-dom/client";
-import { interpolateYlOrRd, scaleSequential, min,max,select,format,range,scaleQuantize} from 'd3';
+import { interpolateYlOrRd, scaleSequential, min,max,select,format,range,scaleQuantize,interpolateBlues} from 'd3';
 import { legendColor } from 'd3-svg-legend'
 import { useMap } from './useMap';
 import { Marks } from './Marks';
 import { useGas } from './useGas'; 
-import { ColorBar } from './ColorBar';
-
 
 
 // const width = 3072;
@@ -34,16 +32,15 @@ const App = () => {
   const colorValue = d => d.diesel;
 
   
-  const colorScale = scaleSequential(interpolateYlOrRd)
+  const colorScale = scaleSequential(interpolateBlues)
 		.domain([min(gas,colorValue),max(gas,colorValue)]);
 
- 
-  console.log(typeof colorScale);
   var svg = select("#colorbar");
+
 
   svg.append("g")
       .attr("class", "legendQuant")
-      .attr("transform", "translate(390,210) scale(0.5)");
+      .attr("transform", "translate(520,70) scale(1)");
   
   var legend = legendColor()
       .labelFormat(format(".3f"))
@@ -59,19 +56,23 @@ const App = () => {
 
   
   return (
-    <svg width={width} height={height}>
-      <svg viewBox="400 200 400 300"
-        >
-          <Marks
-            map={map} 
-            rowByState={rowByState}
-            colorScale = {colorScale}
-            colorValue = {colorValue}
-          />
+    <svg viewBox="0 0 2000 1500">
+      <svg viewBox="500 0 800 1500" id="colorbar">
+        <g transform = "translate(-1050,-650) scale(4)">
+            <Marks
+              map={map} 
+              rowByState={rowByState}
+              colorScale = {colorScale}
+              colorValue = {colorValue}
+            />
+        </g>
       </svg>
-      <svg viewBox="480 200 200 200" id="colorbar">
-      
-      </svg> 
+      <rect x="0" y="0" width="2000" height="60" fill="blue"></rect>
+      <text x="1000" y="40" fontFamily="Verdana" fontSize="35" fill="yellow" textAnchor="middle">Germany gas price visualization</text>
+      <line x1="600" y1="1000" x2="600" y2="60" stroke="black" />
+      <line x1="1350" y1="1000" x2="1350" y2="60" stroke="black" />
+      <rect x="1050" y="70" width="200" height="100" fill="none" stroke="black" rx="15" ></rect>
+      <text x="1150" y="130" fontFamily="Verdana" fontSize="35" fill="yellow" textAnchor="middle" id="statename">State</text>
     </svg>
   );
 };
