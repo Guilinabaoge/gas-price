399f3acbd94143f0637f8b5779ecd221c3674350
Guilinabaoge <42078245+Guilinabaoge@users.noreply.github.com>
2022-12-02 03:12:44 +0100

    why the fk function can't be invoke inside a loop in js?

--- Dataviz-backend/local.mjs
+++ Dataviz-backend/local.mjs
@@ -4,8 +4,6 @@ import pg from "pg"
 
 var rows = null
 
-
-
 async function getStates(){
   const client = new pg.Client({
     user: 'postgres',
@@ -16,20 +14,57 @@ async function getStates(){
   })
   client.connect()
     
-  client.query('select * from perfect where state is null limit 1', (err, res) => {
-    rows = res.rows
-    const state = showRows()
+  client.query("select * from perfect where state is null limit 1", (err, res) => {
+    const state = 
+    showRows(res.rows[0].lat,res.rows[0].lng).then((d)=>{return d.json()})
+    .then((myJson) => {
+      const stid = res.rows[0].stid
+      const state = myJson[0][0].admin1Code.name
+      updateState(state,stid)
+      // console.log(`update perfect set state = '${state}' where stid = '${stid}'`)
+    }
+    )
     client.end()
   })
 }
 
-getStates()
-
-async function showRows(){
-  const url = 'http://localhost:4000/geocode?latitude=50.485900878906&longitude=9.7160587310791'
-  const response = 
-  await fetch(url).then((d)=>{return d.json()}).then((myJson)=> console.log(myJson[0][0].admin1Code))
+async function showRows(lat,lng){
+  const url = `http://localhost:4000/geocode?latitude=${lat}&longitude=${lng}`
+  const response = await fetch(url)
   return response
+}
+
+async function updateState(state,stid){
+  const client = new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'zxc1012m',
+    port: 5432,
+  })
+  client.connect()
+    
+  client.query(`update perfect set state = '${state}' where stid = '${stid}'`, (err, res) => {
+    console.log("update success")
+    client.end()
+  })
+}
+
+//why cannot invoke inside for loop?
+// getStates()
+while (true){
+  getStates()
+}
+
+
+
+
+function wait(ms) {
+  var start = Date.now(),
+      now = start;
+  while (now - start < ms) {
+    now = Date.now();
+  }
 }
 
 
